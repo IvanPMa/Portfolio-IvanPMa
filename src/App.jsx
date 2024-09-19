773e48478351d571a9e25d525a838d8327304890
Ivan Pacheco <pachecomaldonadoivan@gmail.com>
2024-09-19 10:29:04 -0600

    Work section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { Navbar } from "./components/Navbar";
 import { Hero } from "./components/Hero";
 import { Technologies } from "./components/Technologies"
+import { Projects } from "./components/Projects";
 
 const App = () => {
   return (
     <div className="overflow-x-hidden text-stone-300">
       <div className="fixed inset-0 -z-10">
-        <div class="relative h-full w-full bg-neutral-900">
-          <div class="absolute inset-0 bg-fuchsia-400 bg-[size:20px_20px] opacity-20 blur-[100px]">
+        <div className="relative h-full w-full bg-neutral-900">
+          <div className="absolute inset-0 bg-fuchsia-400 bg-[size:20px_20px] opacity-20 blur-[100px]">
           </div>
         </div>
       </div>
@@ -16,6 +17,7 @@ const App = () => {
         <Navbar />
         <Hero />
         <Technologies />
+        <Projects />
       </div>
     </div>
   )
