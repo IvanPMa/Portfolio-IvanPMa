20d5add9e5fdd7974f441f3c3b32c31faac5c025
Ivan Pacheco <pachecomaldonadoivan@gmail.com>
2024-10-07 12:47:05 -0600

    Link contact fixed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,7 @@ export const Contact = () => {
                 <a href="mailto:pachecomaldonadoivan@gmail.com" className="my-4 block">
                     {CONTACT.email}
                 </a>
-                <a href="wa.link/9hx7ez" className="my-4 block">
+                <a href="https://api.whatsapp.com/send?phone=525539763601&text=Hi%20Iv%C3%A1n!%20%F0%9F%91%8B%2C%20I%20have%20seen%20your%20portfolio%20and%20I%20found%20your%20profile%20relevant%20for%20a%20vacancy%20we%20have." className="my-4 block">
                     {CONTACT.phoneNo}
                 </a>
                 <p className="my-4">
