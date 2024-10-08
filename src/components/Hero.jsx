e94ec400df9be97b9b013ff1a2d58b6278c90394
Ivan Pacheco <pachecomaldonadoivan@gmail.com>
2024-10-07 18:04:57 -0600

    Titles colors modified

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ export const Hero = () => {
                         <h2 className="pb-2 text-4xl tracking-tighter lg:text-8xl font-normal">
                             Ivan Pacheco    
                         </h2>
-                        <span className="bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent">Software Engineer</span>
+                        <span className="bg-gradient-to-r from-green-600 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent">Software Engineer</span>
                         <p className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter">
                             {HERO_CONTENT}
                         </p>
@@ -24,8 +24,8 @@ export const Hero = () => {
                             target="_blank"
                             rel="noopener noreferrer"
                             download
-                            className="bg-white rounded-full p-4 text-sm text-stone-800 mb-10"
-                        >Resume / CV </a>
+                            className="text-green-600 rounded-full p-4 text-lg mb-10"
+                        >Get my Resume / CV </a>
                     </div>
                 </div>
             </div>
